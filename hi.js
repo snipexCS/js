abea47d52450ffc4cacf277f8d6feacd5e85cc3c
snipexCS <123857302+snipexCS@users.noreply.github.com>
2023-03-04 16:20:11 +0500

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,14 @@
+let greeting=prompt("имя")
+console.log(" Я "+greeting);
+let age=prompt("возраст")
+console.log(" мне "+age);
+
+let task=prompt("solve 12+5=?")
+console.log('Пример 1: 12 + 5 = 17 ' + " Ваш ответ " + task);
+let minus=prompt("solve 12-5=?")
+console.log('Пример 1: 12 - 5 = 7 ' + " Ваш ответ " + minus);
+let mix=prompt("solve 12*5=?")
+console.log('Пример 1: 12 * 5 = 60 ' + " Ваш ответ " + mix);
+let del=prompt("solve 12/5=?")
+console.log('Пример 1: 12 / 5 = 60 ' + " Ваш ответ " + del );
+alert("Open console")
